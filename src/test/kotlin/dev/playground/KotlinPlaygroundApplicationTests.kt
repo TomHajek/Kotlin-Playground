9f66bc722df27d274bf4c42de51edf580914d301
_hayas <hajcus10@seznam.cz>
2024-11-19 20:15:18 +0100

    Initial commit

---
+++ src/test/kotlin/dev/playground/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
